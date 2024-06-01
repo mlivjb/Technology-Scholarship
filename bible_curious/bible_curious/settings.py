bec9e47f7fc2e4eb63d75d9057d961da7df93fda
Peter Bayne <peter.bayne@haumohio.com>
2024-06-01 16:11:05 +1200

    Re-arrange static folders

--- bible_curious/bible_curious/settings.py
+++ bible_curious/bible_curious/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from os import getenv
+from os import getenv, path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,8 +122,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    path.join(BASE_DIR, 'static')
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
