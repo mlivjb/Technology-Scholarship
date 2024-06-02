c47ec334d5125e5a861e61b4e085b3368a5c164c
Liv Bayne <mojbayne@gmail.com>
2024-06-02 20:43:26 +1200

    Move collections views into app
    collections files into app with urls
    - have to change names of things into variable ie. "moses" and "leaders_of_the_bible"

--- bible_curious/passages/urls.py
+++ bible_curious/passages/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.collection_page("index"), name="index"),
+    path("leaders_of_the_bible", views.collection_page("stories"), name="stories"),
+    path("leaders_of_the_bible/moses", views.collection_page("storyline"), name="storyline"),
+    path("leaders_of_the_bible/moses/1", views.collection_page("question"), name="question"),
+    path("leaders_of_the_bible/moses/2", views.collection_page("passage"), name="passage"),
 ]
--- bible_curious/passages/views.py
+++ bible_curious/passages/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import HttpResponse, render
 
+def collection_page(page_name):
+    def response(request):
+        context = {}
+        return render(request, f"collections/{page_name}.html", context)
+    return response
+
+
 def index(request):
     context = {}
-    return render(request, "passages/index.html", context)
+    return render(request, "collections/index.html", context)
+
+def stories(request):
+    context = {}
+    return render(request, "collections/stories.html", context)
+
+def storyline(request):
+    context = {}
+    return render(request, "collections/stories.html", context)
